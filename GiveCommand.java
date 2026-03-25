5cd8bd308156ed1ce2b936003fcf5bd633ffd282
souaianas33-jpg <souaianas33@gmail.com>
2026-03-25 04:23:28 +0100

    Update GiveCommand.java

--- GiveCommand.java
+++ GiveCommand.java
@@ -1,1 +1,28 @@
+package me.anas.customweapons;
 
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+
+public class GiveCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        Player p = (Player) sender;
+
+        p.getInventory().addItem(
+                ItemManager.meteor(),
+                ItemManager.wardenSword(),
+                ItemManager.frost(),
+                ItemManager.vampire(),
+                ItemManager.combo(),
+                ItemManager.cobweb(),
+                ItemManager.dashMace(),
+                ItemManager.heartMace(),
+                ItemManager.freezeMace()
+        );
+
+        p.sendMessage("§aAll weapons received!");
+        return true;
+    }
+}
