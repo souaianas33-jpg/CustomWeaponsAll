0778684b8913955c4a4cd84e68a68860d361462b
souaianas33-jpg <souaianas33@gmail.com>
2026-03-25 04:22:05 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,25 @@
+package me.anas.customweapons;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    private static Main instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        saveDefaultConfig();
+
+        getServer().getPluginManager().registerEvents(new WeaponListener(), this);
+        getServer().getPluginManager().registerEvents(new GUIListener(), this);
+
+        getCommand("givecustom").setExecutor(new GiveCommand());
+        getCommand("weapons").setExecutor(new GUICommand());
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
+}
