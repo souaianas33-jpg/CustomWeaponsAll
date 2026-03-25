8e339f6551829be07a23f16473fcffaeb486b2f6
souaianas33-jpg <souaianas33@gmail.com>
2026-03-25 04:25:02 +0100

    Update GUIListener.java

--- GUIListener.java
+++ GUIListener.java
@@ -1,1 +1,24 @@
+package me.anas.customweapons;
 
+import org.bukkit.event.*;
+import org.bukkit.event.inventory.*;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class GUIListener implements Listener {
+
+    @EventHandler
+    public void click(InventoryClickEvent e) {
+        if (!e.getView().getTitle().equals("§8Custom Weapons")) return;
+
+        e.setCancelled(true);
+
+        Player p = (Player) e.getWhoClicked();
+        ItemStack item = e.getCurrentItem();
+
+        if (item == null) return;
+
+        p.getInventory().addItem(item.clone());
+        p.sendMessage("§aWeapon received!");
+    }
+}
